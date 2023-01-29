0da405a50874cfec0e17ac74b10475bfdf2081e8
Osmar Pereira <bibliotecaosmar@gmail.com>
2023-01-29 14:54:44 -0300

    Estrutura do site

--- horario_aluno.php
+++ horario_aluno.php
@@ -2,8 +2,10 @@
 
 // Decode do json (GET)
 $data_page = file_get_contents('http://camerascomputex.ddns.net:8080/escola/mobile_login.php?matricula=2011004&senha=99999999&token=X&so=ios');
+$data_horario = file_get_contents('http://camerascomputex.ddns.net:8080/escola/json_horario_aluno.php?matricula=2011004&senha=99999999&ano=20211');
 
 $page = json_decode($data_page);
+$horario = json_decode($data_horario);
 
 // Componentes da página
 $config = $page->config;
@@ -24,3 +26,5 @@ $sti = $page->sti;
 
 
 <?php Include 'footer.php' ?>
+
+
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
- * Sistema escolar em PHP8 com uso de javascript para processamento de DOM e interface amistosa.
+ * Sistema escolar em PHP8 e interface amistosa.
  * Atendemos a padronização de código sempre que possível.
  *
  * || *INDEX* ||
  * Tarefa #1: home.php
- * Tarefa #2:
- * Tarefa #3:
- * Tarefa #4:
+ * Tarefa #2: horario.php
+ * Tarefa #3: acesso.php
+ * Tarefa #4: alunos.php
  *
  */
 
