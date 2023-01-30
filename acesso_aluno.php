9b16a4b6106e4c7153e9338d46feb948fff976f1
Osmar Pereira <bibliotecaosmar@gmail.com>
2023-01-30 09:13:30 -0300

    Tabela 2.

---
+++ acesso_aluno.php
@@ -0,0 +1,65 @@
+<?php
+
+// Decode do json (GET)
+$data_page = file_get_contents('http://camerascomputex.ddns.net:8080/escola/mobile_login.php?matricula=2011004&senha=99999999&token=X&so=ios');
+$data_acesso_aluno = file_get_contents('http://camerascomputex.ddns.net:8080/escola/ws_controller.php?action=getTurmas&ano=20211');
+
+$page = json_decode($data_page);
+$acesso_aluno = json_decode($data_acesso_aluno);
+
+// Componentes da página
+$config = $page->config;
+$menu = $page->menu;
+$escola = $page->escola;
+$proximo_ano = $page->proximo_ano;
+$proxima_serie = $page->proxima_serie;
+$proximo_grau_serie = $page->proximo_grau_serie;
+$boleto_processado = $page->boleto_processado;
+$permissao_matricula = $page->permissao_matricula;
+$data_aceite = $page->data_aceite;
+$sti = $page->sti;
+
+echo gettype($acesso_aluno[0]);
+
+?>
+
+<?php Include 'head.php' ?>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Código da Escola</th>
+      <th scope="col">Ano</th>
+      <th scope="col">Grau da Série</th>
+      <th scope="col">Turno</th>
+      <th scope="col">Turma</th>
+      <th scope="col">Grau Longo</th>
+      <th scope="col">Série Longa</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+    for ($i=0; $i<5; $i++) {
+  
+      $acesso = $acesso_aluno[$i];
+      echo '<tr>';
+      echo '  <th scope="row">' . $i+1 . '</th>';
+      echo '  <td>' . $acesso->codigo_escola . '</td>';
+      echo '  <td>' . $acesso->ano . '</td>';
+      echo '  <td>' . $acesso->grau_serie . '</td>';
+      echo '  <td>' . $acesso->turno . '</td>';
+      echo '  <td>' . $acesso->turma . '</td>';
+      echo '  <td>' . $acesso->grau_longo . '</td>';
+      echo '  <td>' . $acesso->serie_longa . '</td>';
+      echo '</tr>';
+        
+    };
+  ?>
+
+  </tbody>
+
+</table>
+
+<?php Include 'footer.php' ?>
+
