8cfb4075afb9bcdc7a42db02f68ea03d07343948
Osmar Pereira <bibliotecaosmar@gmail.com>
2023-02-12 18:01:41 -0300

    Ajustes no cabeçario da página

--- head.php
+++ head.php
@@ -8,66 +8,40 @@
   </head>
   <body>
 
-    <nav class="navbar bg-primary bg-primary-text">
-      <div class="container-fluid">
-        <div class="row align-text-center">
+    <nav class="navbar bg-primary bg-primary-text shadow mb-2 bg-body-primary" data-bs-theme="dark">
+      <div class="container-fluid text-center">
+        <div class="row w-50">
+          <button class="navbar-toggler col-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <div class="col-2">
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+            <img src="imgs/logotipo.png" alt="" width="60" height="60" class="d-inline-block align-text-center">
           </div>
-          <div class="col-2">
-            <img src="imgs/logotipo.png" alt="" width="50" height="50" class="d-inline-block align-text-center">
+          <span class="navbar-brand fs-3 col-2 pt-2" href="#">
+            Computex
+          </span>
+          <a id="Home" class="col-1 pt-3 nav-link fs-5 bg-primary-text" href="home.php">Home</a>
+          <a id="Turma" class="col-1 pt-3 nav-link fs-5 bg-primary-text" href="turma.php">Turma</a>
+          <div class="offcanvas offcanvas-start col-5" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasScrollingLabel">
+                <img src="imgs/logotipo.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
+                <h3 class="bg-primary-text">Computex<h3>
+              </h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
-          <div class="col-4 pt-1">
-            <span class="navbar-brand fs-4" href="#">
-              Computex
-            </span>
+          <div class="offcanvas-body">
+            <div class="list-group list-group-flush">
+              <?php
+                for ($i=0; $i<17; $i++) {
+                  echo '<a type="button" class="list-group-item list-group-item-action" href=' . $menu[$i]->link . '>' . '<img src=imgs/' . $menu[$i]->icone . ' width=20 height=20 class="d-inline-block align-text-top"> ' . $menu[$i]->titulo . ' - ' . $menu[$i]->opc . '</a>';
+                }; 
+              ?>
+            </div>
           </div>
-          <div class="col-2 pt-2">
-            <a class="nav-link fs-5" href="home.php">Home</a>
-          </div>
-          <div class="col-2 pt-2">
-            <a class="nav-link fs-5" href="turma.php">Turma</a>
-          </div>
-        </div>
-      </nav>
-   
-    <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">
-          <img src="imgs/logotipo.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
-          <h3 class="bg-primary-text">Computex<h3>
-        </h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <div class="list-group list-group-flush">
-            <?php
-              for ($i=0; $i<17; $i++) {
-                echo '<a type="button" class="list-group-item list-group-item-action" href=' . $menu[$i]->link . '>' . '<img src=imgs/' . $menu[$i]->icone . ' width=20 height=20 class="d-inline-block align-text-top"> ' . $menu[$i]->titulo . ' - ' . $menu[$i]->opc . '</a>';
-              }; 
-            ?>
         </div>
       </div>
     </div>
+  </nav>
 
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
-      <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasWithBackdropLabel">Offcanvas with backdrop</h5>
-      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-      <div class="offcanvas-body">
-        <p>.....</p>
-      </div>
-    </div>
-
-    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdroped with scrolling</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <p>Try scrolling the rest of the page to see this option in action.</p>
-      </div>
-    </div>
+ 
---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const currentPage = () => {
+  if (document.title = "Home") {
+    document.getElementById("Home").classList.add('active')
+    document.getElementById("Home").classList.remove('disabled')
+
+    document.getElementById("Turma").classList.remove('active')
+    document.getElementById("Turma").classList.add('disabled')
+  } 
+  if (document.title = "Turma") {
+    document.getElementById("Home").classList.remove('active')
+    document.getElementById("Home").classList.add('disabled')
+
+    document.getElementById("Turma").classList.add('active')
+    document.getElementById("Turma").classList.remove('disabled')
+  }
+}
+
+// Default functions
+currentPage()
